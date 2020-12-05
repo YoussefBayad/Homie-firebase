fb391031107a74ca8ec166804b8a9ac7dd947435
Joseph bayad <jobayad66@gmail.com>
2020-12-05 15:09:04 +0100

    search page UI

--- src/features/search/SearchResults.js
+++ src/features/search/SearchResults.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import UsernameAndDate from '../../components/UsernameAndDate';
-
+import './style.scss';
 const SearchResults = ({ posts, users, query }) => {
   return (
     <div className='search-results'>
@@ -8,8 +8,14 @@ const SearchResults = ({ posts, users, query }) => {
         <h1>users</h1>
         <div className='search-users-results'>
           {users.length > 0 &&
-            users.map((user) => <UsernameAndDate {...user} />)}
-          {users.length === 0 && <p> There is no user such "{query}"</p>}
+            users.map((user) => (
+              <div className='user-result'>
+                <UsernameAndDate {...user} />
+              </div>
+            ))}
+          {users.length === 0 && (
+            <p className='no-result'> no match for "{query}"</p>
+          )}
         </div>
       </div>
       <div className='search-posts'>
@@ -17,12 +23,14 @@ const SearchResults = ({ posts, users, query }) => {
         <div className='search-posts-results'>
           {posts.length > 0 &&
             posts.map((post) => (
-              <>
-                <h1>{post.user.displayName}</h1>
-                <p>{post.content}</p>
-              </>
+              <div className='post-result'>
+                <UsernameAndDate {...post.user} />{' '}
+                <p className='post-content'>{post.content}</p>
+              </div>
             ))}
-          {posts.length === 0 && <p> no post contains "{query}"</p>}
+          {posts.length === 0 && (
+            <p className='no-result'> no post contains "{query}"</p>
+          )}
         </div>
       </div>
     </div>
